683bfc66afc5921c065dcdc29e370fd3bc0228d3
jasonB221 <jasonB221@gmail.com>
2014-01-04 00:17:40 -0600

    Added code to close connection

--- src/com/tnmserver/countries/Countries.java
+++ src/com/tnmserver/countries/Countries.java
@@ -2,6 +2,7 @@ package com.tnmserver.countries;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.logging.Level;
 
 import net.milkbowl.vault.permission.Permission;
@@ -9,14 +10,34 @@ import net.milkbowl.vault.permission.Permission;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.tnmserver.countries.sql.Connector;
+
 public class Countries extends JavaPlugin {
 	
 	public static Permission perms = null;
+	private Connector c;
+	
+	public static String GROUP_REFERENCER = "group_name";
+	public static String MEMBER_REFERENCER = "player_list";
+	public static String PERMISSION_REFERENCER = "permissions";
+	public static String IDENTIFIER = "ID";
 
 	@Override
 	public void onEnable(){
 		initVaultPerms();
 		loadConfig();
+		c = new Connector(this);
+	}
+	
+	@SuppressWarnings("static-access")
+	@Override
+	public void onDisable(){
+		try {
+			c.getConnection().close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private void initVaultPerms(){
