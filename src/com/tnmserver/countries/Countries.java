b0684d5c9e4aae498d78b7942740fde9693aa1a9
jasonB221 <jasonB221@gmail.com>
2014-01-03 18:34:32 -0600

    Implemented Vault
    Added code to main plugin page, and added vault requirement at
    plugin.yml

--- src/com/tnmserver/countries/Countries.java
+++ src/com/tnmserver/countries/Countries.java
@@ -1,12 +1,30 @@
 package com.tnmserver.countries;
 
+import java.util.logging.Level;
+
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Countries extends JavaPlugin {
+	
+	public static Permission perms = null;
 
 	@Override
 	public void onEnable(){
-		
+		initVaultPerms();
+	}
+	
+	private void initVaultPerms(){
+		RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+		if(rsp != null){
+			perms = rsp.getProvider();
+		}
+		else{
+			getServer().getLogger().log(Level.SEVERE, "Could not hook into Vault for Perms. Plugin disabling");
+			getPluginLoader().disablePlugin(this);
+		}
 	}
 	
 }
